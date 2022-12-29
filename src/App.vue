790b3ecf8c7cea10bf5352076ed46285c7a77322
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2022-12-29 15:05:40 +0700

    feat(coach-finder): The header init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <the-header></the-header>
+    <router-view></router-view>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+    components: {
+        TheHeader,
+    }
+}
+</script>
+
+<style>
+    
+</style>
